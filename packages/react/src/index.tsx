bb0175e426927a5b83327a499be2c3eb78d1fd08
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-29 21:05:40 -0300

    Ignite UI - Design System em React - Configurando pacote do React

---
+++ packages/react/src/index.tsx
@@ -0,0 +1,13 @@
+import { colors } from '@ignite-ui/tokens'
+
+export function App() {
+  return (
+    <h1
+      style={{
+        color: colors.ignite300,
+      }}
+    >
+      Hello from React!
+    </h1>
+  )
+}
