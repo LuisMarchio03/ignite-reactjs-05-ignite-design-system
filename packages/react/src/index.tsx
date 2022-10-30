bde8ac482ff61f43c6b36fffc61b75ce4153cbde
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-30 10:18:10 -0300

    Ignite UI - Design System em React - Story: Button

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,13 +1,31 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
-  backgroundColor: '$ignite500',
+  backgroundColor: '$ignite300',
   borderRadius: '$md',
-  padding: '$4',
-  height: '$10',
+  border: 0,
+  fontWeight: '$bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>Click me</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
