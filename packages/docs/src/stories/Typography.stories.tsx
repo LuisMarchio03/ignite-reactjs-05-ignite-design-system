e7d4f1a2e297156890a96239f556efaa2bb45703
LuisMarchio03 <luisgabrielmarchio75@gmail.com>
2022-10-31 10:29:37 -0300

    Ignite UI - Componentes do Design System - Componente: Text

---
+++ packages/docs/src/stories/Typography.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: 'Example text',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
